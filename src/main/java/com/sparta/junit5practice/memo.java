5c5e5b3e217a0bcc98dbf6d3ce47cd583865b0d7
namji <skawlgns07@naver.com>
2024-02-21 09:12:02 +0900

    junit test given-when-then
    junit test 시 사용하는 기본 given-when-then 작성 방법

---
+++ src/main/java/com/sparta/junit5practice/memo.java
@@ -0,0 +1,9 @@
+package com.sparta.junit5practice;
+
+public class memo {
+    /*
+    todo: junit test guide
+    todo: https://junit.org/junit5/docs/current/user-guide/#writing-tests-annotations
+     */
+
+}
